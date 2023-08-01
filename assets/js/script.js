62f9ff716e5d493ba4258e276ba43b955c17d38d
Elrond-Hubbard <136862666+Elrond-Hubbard@users.noreply.github.com>
2023-08-01 16:35:14 -0400

    reset

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 
 ///// OSCILLATOR MODULE /////
 
-// WAVEFORM CONTROL
+// WAVEFORM CON
 const waveSelector = document.querySelector('#wave-selector');
 const waveLabel = document.getElementById('wave-label')
 const waveForms = [
@@ -26,6 +26,22 @@ function Oscillator() {
     osc.stop(actx.currentTime + 0.5);
 }
 
-// TRIGGER
-const playButton = document.querySelector('#play');
-playButton.addEventListener('click', Oscillator);
+// TRIGGERS
+const NOTES = {
+    'c-4': 261.626,
+    'c#4': 277.183,
+    'd-4': 293.665,
+    'd#4': 311.127,
+    'e-4': 329.628,
+    'f-4': 349.228,
+    'f#4': 369.994,
+    'g-4': 391.995,
+    'g#4': 415.305,
+    'a-4': 440,
+    'a#4': 466.164,
+    'b-4': 493.883,
+    'c-5': 523.251
+}
+
+playButton = document.querySelector('#button')
+playButton.addEventListener('click', Oscillator)
