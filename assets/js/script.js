7fc22d14c2cf7b12a3de3dad90546589daeb70de
Elrond-Hubbard <136862666+Elrond-Hubbard@users.noreply.github.com>
2023-08-01 14:01:47 -0400

    added waveform control

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,26 @@
 
 
-playButton = document.querySelector('#play');
-playButton.addEventListener('click', playTone);
+// WAVEFORM CONTROL
+const waveSelector = document.querySelector('#wave-selector');
+const waveLabel = document.getElementById('wave-label')
+const waveForms = [
+    'sine',
+    'square',
+    'sawtooth',
+    'triangle'
+];
+waveSelector.addEventListener('change', function() {
+    waveLabel.innerText = waveForms[waveSelector.value];
+})
 
+// OSCILLATOR TRIGGER
+const playButton = document.querySelector('#play');
+playButton.addEventListener('click', playTone);
 function playTone() {
     const actx = new (AudioContext);
     const osc = actx.createOscillator();
-    osc.type = 'sawtooth';
-    osc.frequency.value = 440; //Hz = middle A
+    osc.type = waveForms[waveSelector.value];
+    osc.frequency.value = 220; //Hz = bass A
     osc.connect(actx.destination); //pc audio output
     osc.start();
     osc.stop(actx.currentTime + 0.5);
