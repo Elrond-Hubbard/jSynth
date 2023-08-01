7a66d12df2d44ff418e0ee7e9463a9fd7a943722
Elrond-Hubbard <136862666+Elrond-Hubbard@users.noreply.github.com>
2023-08-01 18:26:53 -0400

    wrote selector for each key

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-///// OSCILLATOR MODULE /////
-
-// WAVEFORM
-const waveSelector = document.querySelector('#wave-selector');
-const waveLabel = document.getElementById('wave-label')
-const WAVEFORM = [
-    'sine',
-    'square',
-    'sawtooth',
-    'triangle'
-];
-waveSelector.addEventListener('input', function() {
-    waveLabel.innerText = WAVEFORM[waveSelector.value];
-})
-
-// UNISON
-const unisonSlider = document.querySelector('#unison-width');
-var unisonWidth = unisonSlider.value
-unisonSlider.addEventListener('input', function() {
-    unisonWidth = unisonSlider.value;
-})
-
-// OSCILLATORS
-oscBank = new Array(3);
-function createOscillator(detune) {
-    const actx = new (AudioContext || webkitAudioContext());
-    const osc = actx.createOscillator();
-    osc.type = WAVEFORM[waveSelector.value];
-    osc.frequency.value = 440; //Hz = bass A
-    osc.detune.value = detune;
-    osc.connect(actx.destination); //pc audio output
-    osc.start();
-    osc.stop(actx.currentTime + 0.5);
-}
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 ///// KEY CONTROLLER MODULE /////
 
 // TRIGGERS
@@ -57,9 +19,52 @@ const NOTES = {
     'c-5': 523.251
 }
 
-playButton = document.querySelector('#play')
-playButton.addEventListener('mousedown', function() {
-    oscBank[0] = createOscillator(0);
-    oscBank[1] = createOscillator(unisonWidth);
-    oscBank[2] = createOscillator(-unisonWidth);
-});
+document.querySelectorAll('button[data-note]').forEach((button) => {
+    const freq = (NOTES[button.dataset.note])
+    button.addEventListener('click', function () {
+        oscBank[0] = createOscillator(freq, 0);
+        oscBank[1] = createOscillator(freq, unisonWidth);
+        oscBank[2] = createOscillator(freq, -unisonWidth);
+    })
+})
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+///// OSCILLATOR MODULE /////
+
+// WAVEFORM
+const waveSelector = document.querySelector('#wave-selector');
+const waveLabel = document.getElementById('wave-label')
+const WAVEFORM = [
+    'sine',
+    'square',
+    'sawtooth',
+    'triangle'
+];
+waveSelector.addEventListener('input', function () {
+    waveLabel.innerText = WAVEFORM[waveSelector.value];
+})
+
+// UNISON
+const unisonSlider = document.querySelector('#unison-width');
+var unisonWidth = unisonSlider.value
+unisonSlider.addEventListener('input', function () {
+    unisonWidth = unisonSlider.value;
+})
+
+// OSCILLATORS
+oscBank = new Array(3);
+function createOscillator(freq, detune) {
+    const actx = new (AudioContext || webkitAudioContext());
+    const osc = actx.createOscillator();
+    osc.type = WAVEFORM[waveSelector.value];
+    osc.frequency.value = freq; //Hz = bass A
+    osc.detune.value = detune;
+    osc.connect(actx.destination); //pc audio output
+    osc.start();
+    osc.stop(actx.currentTime + 5);
+    // return osc;
+}
+
+
