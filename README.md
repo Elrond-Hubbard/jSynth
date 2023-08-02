90632b4dd48b9eec23297226d14279fff7601d4a
Elrond-Hubbard <136862666+Elrond-Hubbard@users.noreply.github.com>
2023-08-02 11:21:03 -0400

    added gain envelope

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # jSynth
+[Click Here]()
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
-const actx = new (AudioContext || webkitAudioContext());
-///// KEY CONTROLLER MODULE /////
-
-// NOTE FREQUENCY BANK
-const NOTES = {
-    'c-4': 261.626,
-    'c#4': 277.183,
-    'd-4': 293.665,
-    'd#4': 311.127,
-    'e-4': 329.628,
-    'f-4': 349.228,
-    'f#4': 369.994,
-    'g-4': 391.995,
-    'g#4': 415.305,
-    'a-4': 440,
-    'a#4': 466.164,
-    'b-4': 493.883,
-    'c-5': 523.251
-}
-
-let keyPressed = false;
-
-// TRIGGER OSCILLATOR
-document.querySelectorAll('button[data-note]').forEach((button) => {
-    const freq = (NOTES[button.dataset.note])
-    button.addEventListener('pointerdown', function (event) {
-        event.preventDefault;
-        oscBank = new Array(3);
-        oscBank[0] = createOscillator(freq, 0, keyPressed);
-        oscBank[1] = createOscillator(freq, unisonWidth, keyPressed);
-        oscBank[2] = createOscillator(freq, -unisonWidth, keyPressed);
-        oscBank.forEach(osc => {
-            osc.start();
-        })
-    })
-    button.addEventListener('pointerup', function (event) {
-        event.preventDefault;
-        oscBank.forEach(osc => osc.stop());
-    })
-})
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+const actx = new (window.AudioContext || webkitAudioContext)();
 ///// OSCILLATOR MODULE /////
 
 // WAVEFORM
@@ -66,11 +23,68 @@ unisonSlider.addEventListener('input', function () {
 })
 
 // OSCILLATORS
-function createOscillator(freq, detune) {
+function oscillatorModule(freq, detune) {
     const osc = actx.createOscillator();
     osc.type = WAVEFORM[waveSelector.value];
     osc.frequency.value = freq;
     osc.detune.value = detune;
-    osc.connect(actx.destination)
+    osc.connect(env)
     return osc;
 }
+
+
+const env = actx.createGain();
+env.connect(actx.destination);
+var decay = 0.75;
+var attack = 0.5;
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+///// KEY CONTROLLER MODULE /////
+
+// NOTE FREQUENCY BANK
+const NOTES = {
+    'c-4': 261.626,
+    'c#4': 277.183,
+    'd-4': 293.665,
+    'd#4': 311.127,
+    'e-4': 329.628,
+    'f-4': 349.228,
+    'f#4': 369.994,
+    'g-4': 391.995,
+    'g#4': 415.305,
+    'a-4': 440,
+    'a#4': 466.164,
+    'b-4': 493.883,
+    'c-5': 523.251
+}
+
+
+// TRIGGER OSCILLATOR
+document.querySelectorAll('button[data-note]').forEach((button) => {
+    const freq = (NOTES[button.dataset.note] * 0.5);
+    button.addEventListener('pointerdown', function (event) {
+        event.preventDefault;
+        event.stopPropagation();
+        oscBank = new Array(3);
+        oscBank[0] = oscillatorModule(freq, 0);
+        oscBank[1] = oscillatorModule(freq, unisonWidth);
+        oscBank[2] = oscillatorModule(freq, -unisonWidth);
+        env.gain.cancelScheduledValues(actx.currentTime);
+        env.gain.setValueAtTime(0, actx.currentTime);
+        env.gain.linearRampToValueAtTime(1, actx.currentTime + attack);
+        oscBank.forEach(osc => {
+            osc.start(actx.currentTime);
+        })
+    })
+    button.addEventListener('pointerup', function (event) {
+        event.preventDefault;
+        oscBank.forEach(osc => {
+            event.preventDefault();
+            event.stopPropagation();
+            env.gain.linearRampToValueAtTime(0, actx.currentTime + decay)
+            osc.stop(actx.currentTime + decay);
+        });
+    })
+})
